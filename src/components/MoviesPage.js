d523bc95668d8e371a24de8eff8cd6b181c4df5c
Latoshia Williams <lswillms@yahoo.com>
2022-11-24 10:53:30 -0600

    commit tree 1886cf5b249887661460902787880277d2798812

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+  
+  const match = useRouteMatch()
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route exact path = {`${match.url}`}/>
+      <Route path = {`${match.url}/:moviedId`}/>
+      <MovieShow movies = {movies}/>
     </div>
   );
 }
